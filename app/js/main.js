f3bb7dc69bf5306f9740700037f9e444243b826e
DimiG <dim.georgiev1976@yandex.com>
2019-01-04 14:58:28 +0000

    new script for vaidate-form && new settings

--- app/js/main.js
+++ app/js/main.js
@@ -61,10 +61,11 @@ $(document).ready(function(){
         }
       }
    //slide2id - плавная прокрутка по ссылкам внутри страницы
+   
       $("li a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
           highlightSelector:"nav a"
       });
 
-    
+   
 
 	 });
