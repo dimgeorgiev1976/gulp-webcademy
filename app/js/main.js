efe91c1def9648d9383626bb3df80c2efefe3066
DimiG <dim.georgiev1976@yandex.com>
2019-01-11 08:26:44 +0000

    plugin /page-scroll-to-id/

--- app/js/main.js
+++ app/js/main.js
@@ -61,9 +61,10 @@ $(document).ready(function(){
         }
       }
    //slide2id - плавная прокрутка по ссылкам внутри страницы
-   
+      
       $("li a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
-          highlightSelector:"nav a"
-      });
+          highlightSelector:"nav a",
+          scrollSpeed:1200
+         });
 
     });
